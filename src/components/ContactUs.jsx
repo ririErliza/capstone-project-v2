1e28d6355b853459e69b0b50a8ec3a186cff1886
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-07-06 12:41:43 +0200

    update

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -19,18 +19,18 @@ const ContactUs = () => {
                   
                   <div className="form-cu">
                   <Form className='mx-4 my-4'>
-                  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Your Email</Form.Label>
-                    <Form.Control type="email" className="w-100"/>
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Your Name</Form.Label>
-                    <Form.Control type="text" className="w-100"/>
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Your Message</Form.Label>
-                    <Form.Control as="textarea" rows={3} className="w-100" />
-                  </Form.Group>
+                      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Your Email</Form.Label>
+                        <Form.Control type="email" name="email" className="w-100"/>
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Your Name</Form.Label>
+                        <Form.Control type="text" name="name" className="w-100"/>
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Your Message</Form.Label>
+                        <Form.Control as="textarea" name="message" rows={3} className="w-100" />
+                      </Form.Group>
                 </Form>
 
                 <Button className="button-contact mb-1 mb-sm-0 mx-5 my-4">Send</Button>
@@ -47,15 +47,6 @@ const ContactUs = () => {
                 <div className='mr-2'> <FaWhatsappSquare color='#087776' size='4rem'/> </div>
                 
               </Col>
-
-                {/* <Col xs={12} md={5}>
-                <ul id="social-buttons" className="py-3 px-3">
-                    <li className='pb-3'> <FaFacebookSquare color='#087776' size='3rem'/> </li>
-                    <li className='pb-3'> <FaInstagramSquare color='#087776' size='3rem'/></li>
-                    <li className='pb-3'> <MdEmail color='#087776' size='3rem'/> </li>
-                    <li className='pb-3'> <FaWhatsappSquare color='#087776' size='3rem'/> </li>
-                  </ul>
-                </Col> */}
 
              
             </Row>
