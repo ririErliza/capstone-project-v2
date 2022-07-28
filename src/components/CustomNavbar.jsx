f1b5ac73c0d7a11e44c4c96a78aff47bd42ec5f7
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-07-28 11:51:56 +0200

    Cart Slice

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Navbar, Nav, Badge } from 'react-bootstrap';
 import logo from '../img/logo.png';
 import { Link, useLocation } from 'react-router-dom'
@@ -7,9 +7,10 @@ import { FaShoppingBasket } from "react-icons/fa"
 
 function CustomNavbar() {
   const location = useLocation()
+  const [expanded, setExpanded] = useState(false);
   return (
     <section id="nav-bar">
-    <Navbar collapseOnSelect expand="lg" className="Navbar navbar-dark">
+    <Navbar expanded={expanded} collapseOnSelect expand="lg" className="Navbar navbar-dark">
   <Navbar.Brand>
     <Link to="/">
   <img
@@ -21,28 +22,33 @@ function CustomNavbar() {
       />
       </Link>
   </Navbar.Brand>
-  <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
+  <Navbar.Toggle  onClick={() => setExpanded(expanded? false : "expanded")} aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse  id="responsive-navbar-nav">
     <Nav className="ml-auto">
 
-    <Link to="/" className='text-white mr-4 '> <div className={
+    <Link onClick={() => setExpanded(false)}
+               to="/" className='text-white mr-4 '> <div className={
                 location.pathname === '/' ? 'nav-link active' : 'nav-link'
               }> HOME </div>  </Link>
-    <Link to="/aboutUs" className='text-white mr-4 '> <div className={
+    <Link onClick={() => setExpanded(false)}
+               to="/aboutUs" className='text-white mr-4 '> <div className={
                 location.pathname === '/aboutUs' ? 'nav-link active' : 'nav-link'
               }> ABOUT US </div> </Link>
 
-    <Link to="/contactus" className='text-white mr-4 '> <div className={
+    <Link onClick={() => setExpanded(false)}
+              to="/contactus" className='text-white mr-4 '> <div className={
                 location.pathname === '/contactus' ? 'nav-link active' : 'nav-link'
               } > CONTACT US </div> </Link>
 
 
 
-<Link to="/shop" className='text-white mr-4 '> <div className={
+    <Link onClick={() => setExpanded(false)}
+              to="/shop" className='text-white mr-4 '> <div className={
                 location.pathname === '/shop' ? 'nav-link active' : 'nav-link'
               }> SHOP </div>  </Link>
 
-<Link to="/cart" className='text-white mr-4 '> <div className={
+    <Link onClick={() => setExpanded(false)}
+             to="/cart" className='text-white mr-4 '> <div className={
                 location.pathname === '/cart' ? 'nav-link active' : 'nav-link'
               }> <FaShoppingBasket style={{ fontSize:'1.6em', color:'#ffff' }}/>
               <Badge className='badge badge-danger rounded-circle' id="badge-number"> 1 </Badge> </div>  </Link>
